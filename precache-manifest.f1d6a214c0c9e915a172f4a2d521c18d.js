884bf18bc88bcc0c7f0c628b5f3559997d4d0a18
Anthony Robin <robinjly@hotmail.com>
2019-02-20 17:12:22 -0800

    Updates

---
+++ precache-manifest.f1d6a214c0c9e915a172f4a2d521c18d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcd5d093ada1224f6df8",
+    "url": "/portfolio/static/css/main.589a3286.chunk.css"
+  },
+  {
+    "revision": "bcd5d093ada1224f6df8",
+    "url": "/portfolio/static/js/main.bcd5d093.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "3d72b2baeaf723413c4e",
+    "url": "/portfolio/static/css/2.2edd7788.chunk.css"
+  },
+  {
+    "revision": "3d72b2baeaf723413c4e",
+    "url": "/portfolio/static/js/2.3d72b2ba.chunk.js"
+  },
+  {
+    "revision": "d75fd361aefef7b61a0941f434118779",
+    "url": "/portfolio/static/media/leaf.d75fd361.jpg"
+  },
+  {
+    "revision": "ae45fbcd91c40b357a48336b632a95f1",
+    "url": "/portfolio/index.html"
+  }
+];
